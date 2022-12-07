ced8d637cf1805190e1b8546eb700920d4d24f5f
xinuior <juniorcn2@hotmail.com>
2022-12-07 20:05:13 -0300

    :tada: Commit inicial

---
+++ README.md
@@ -0,0 +1,4 @@
+<h1 align="center">Barra de navegação com animação | HTML - CSS - JAVASCRIPT</h1>
+<div align="center">
+    <img src="img/navBarAnimation.png" alt="Barra de navegação com animação">
+</div>
---
+++ script.js
@@ -0,0 +1,35 @@
+const uls = document.querySelectorAll("nav ul");
+const links = [...document.querySelectorAll("nav a")];
+const light = document.querySelector("nav .tubelight");
+let activeIndex = 0;
+let currentIndex = 0;
+let increment = 1;
+links.forEach((link, index) => 
+{
+	if (links[index].classList.contains("active"))
+    {
+		light.style.left = `${links[index].offsetLeft + light.offsetWidth / 4}px`;
+	}
+	link.addEventListener("click", (e) => 
+    {
+		activeIndex = index;
+		let t = setInterval(() => 
+        {
+			if (activeIndex > currentIndex) increment = 1;
+			else if (activeIndex < currentIndex) increment = -1;
+			currentIndex += increment;
+
+			links[currentIndex].classList.add("active");
+			if (currentIndex != -1)
+				links[currentIndex - increment].classList.remove("active");
+
+			if (currentIndex == activeIndex) 
+            {
+				e.target.classList.add("active");
+				increment = 0;
+				clearInterval(t);
+			}
+		}, 50);
+		light.style.left = `${e.target.offsetLeft + light.offsetWidth / 4}px`;
+	});
+});
